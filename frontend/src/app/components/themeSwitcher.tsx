905aeb605f3c848e5ae34f6e3647aed325872ea1
Cristian Mozetich <cristianmozetich@gmail.com>
2024-06-30 21:35:43 +0100

    next-themes

--- frontend/src/app/components/themeSwitcher.tsx
+++ frontend/src/app/components/themeSwitcher.tsx
@@ -1,23 +1,33 @@
 // app/components/ThemeSwitcher.tsx
 "use client";
-
-import {useTheme} from "next-themes";
+import { button } from "@nextui-org/react";
+import MoonIcon from "../icons/MoonIcon";
+import SunIcon from "../icons/SunIcon";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import "../globals.css";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if(!mounted) return null
+  if (!mounted) return null;
 
   return (
-    <div className="flex gap-2">
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
+    <div className="flex gap-2 transition-icon">
+      {theme === "light" ? (
+        <button onClick={() => setTheme("dark")}>
+          <MoonIcon />
+        </button>
+      ) : (
+        <button onClick={() => setTheme("light")}>
+          <SunIcon />
+        </button>
+      )}
     </div>
-  )
-};
+  );
+}
