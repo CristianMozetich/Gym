c9073c6fd31ab6d84bd84191bcf2fe08bbb6c59c
Cristian Mozetich <cristianmozetich@gmail.com>
2024-11-02 00:13:42 +0000

    pixel remove

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,29 +24,6 @@ export default function RootLayout({
   const fbPixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
   return (
     <html lang="en">
-     <head>
-        {/* Añadimos el Script del Pixel de Meta en el head */}
-        {fbPixelId && (
-          <Script
-            id="fb-pixel"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${fbPixelId}');
-                fbq('track', 'PageView');
-              `,
-            }}
-          />
-        )}
-      </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
       <noscript>
           <Image
